4e52aeeee4a8a1e0e2a551b8c26e55b727b0a48f
DimasHN_04 <dimashn04@gmail.com>
2024-04-05 10:23:35 +0700

    Removed any unused imports

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -2,8 +2,6 @@ use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
 
-use super::product;
-
 // Singleton of DB
 lazy_static! {
     pub static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
